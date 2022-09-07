d391db4275b44edc31c3cd82e0ec42f6d26c1a1c
KILLERAK-7 <shakthebarane@gmail.com>
2022-09-07 13:12:15 +0530

    Forgot COmmit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import logo from "../logo.svg";
 import "./App.css";
 import Chat from "./Chat";
+import Waves from "./Waves";
 
 function App() {
   return (
     <div className="App">
       <Chat />
+      <Waves />
     </div>
   );
 }
